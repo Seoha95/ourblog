4019d6b61e90126bd6b3212c10e8a0f57abbb7b3
Seoha95 <serha95@naver.com>
2024-02-14 22:36:40 +0900

    Refactor: HeartServiceTest 리펙토링

--- src/test/java/com/team/ourblog/service/HeartServiceTest.java
+++ src/test/java/com/team/ourblog/service/HeartServiceTest.java
@@ -3,8 +3,10 @@ package com.team.ourblog.service;
 import com.team.ourblog.dto.request.posting.PostingRequestDto;
 import com.team.ourblog.dto.response.posting.PostingResponseDto;
 import com.team.ourblog.entity.Authority;
+import com.team.ourblog.entity.Category;
 import com.team.ourblog.entity.Heart;
 import com.team.ourblog.entity.Member;
+import com.team.ourblog.repository.CategoryRepository;
 import com.team.ourblog.repository.MemberRepository;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
@@ -30,48 +32,57 @@ class HeartServiceTest {
     @Autowired
     HeartService heartService;
 
-    private PostingResponseDto dto;
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    private PostingResponseDto responseDto;
+    private PostingResponseDto responseDto2;
     private Member testMember;
     private Member testMember2;
 
     @BeforeEach
     void setUp(){
-        testMember = new Member();
-        testMember.setEmail("test11@naver.com");
-        testMember.setPassword("dltjgk19950322@");
-        testMember.setName("테스트일일");
-        testMember.setNickname("닉네임일일");
-        testMember.setAuthority(Authority.ROLE_USER);
-
+        testMember = createMember(2L,"test10@naver.com", "dltjgk19950322@","테스트","닉네임십");
         memberRepository.save(testMember);
 
-        testMember2 = new Member();
-        testMember2.setEmail("test12@naver.com");
-        testMember2.setPassword("dltjgk19950322@");
-        testMember2.setName("테스트이이");
-        testMember2.setNickname("닉네임이이");
-        testMember2.setAuthority(Authority.ROLE_USER);
-
+        testMember2 = createMember(3L,"test11@naver.com", "dltjgk19950322@","테스트십일","닉네임십일");
         memberRepository.save(testMember2);
 
-        PostingRequestDto requestDto = new PostingRequestDto();
-        requestDto.setTitle("테스트제목");
-        requestDto.setContent("테스트내용");
-        requestDto.setNickName("닉네임일일");
-        postingService.createPosting(requestDto, testMember.getId());
+        Category category = createCategory("음식", testMember);
+        responseDto = createPosting(String.valueOf(category.getId()),"테스트제목","테스트내용",testMember.getNickname());
 
-        PostingRequestDto requestDto2 = new PostingRequestDto();
-        requestDto2.setTitle("테스트제목2");
-        requestDto2.setContent("테스트내용2");
-        requestDto2.setNickName("닉네임이이");
-        dto = postingService.createPosting(requestDto2, testMember2.getId());
+        Category category2 = createCategory("취미", testMember2);
+        responseDto2 =  createPosting(String.valueOf(category2.getId()),"테스트제목2","테스트내용2",testMember2.getNickname());
+    }
+
+    private Member createMember(Long id,String email, String password, String userName, String nickname) {
+        Member member =  new Member(id,email,nickname,userName,password,Authority.ROLE_USER);
+        return memberRepository.save(member);
+
+    }
+
+    private PostingResponseDto createPosting(String categoryId,String title, String content, String autor) {
+        PostingRequestDto requestDto = new PostingRequestDto();
+        requestDto.setCategoryId(categoryId);
+        requestDto.setTitle(title);
+        requestDto.setContent(content);
+        requestDto.setNickname(autor);
+
+        return postingService.createPosting(requestDto, testMember.getId());
+    }
+
+    private Category createCategory(String categoryName, Member member) {
+        Category category = new Category();
+        category.setCategoryName(categoryName);
+        category.setMember(member);
+        return categoryRepository.save(category);
     }
 
     @Test
     void heartInsert() {
 
-        Heart heart = heartService.heartInsert(dto.getPostId(), testMember.getId());
-        Heart heart1 = heartService.heartInsert(dto.getPostId(), testMember2.getId());
+        Heart heart = heartService.heartInsert(responseDto.getPostId(), testMember.getId());
+        Heart heart1 = heartService.heartInsert(responseDto2.getPostId(), testMember2.getId());
 
         assertThat(heart.getId()).isNotNull();
         assertThat(heart1.getId()).isNotNull();
@@ -81,14 +92,14 @@ class HeartServiceTest {
     @Test
     void heartDelete() {
 
-        heartService.heartInsert(dto.getPostId(), testMember.getId());
-        heartService.heartInsert(dto.getPostId(), testMember2.getId());
+        heartService.heartInsert(responseDto.getPostId(), testMember.getId());
+        heartService.heartInsert(responseDto2.getPostId(), testMember2.getId());
 
-        HashMap<String, Object> hashMap =  heartService.getHeartUser(dto.getPostId(),testMember2.getId());
+        HashMap<String, Object> hashMap =  heartService.getHeartUser(responseDto.getPostId(),testMember2.getId());
 
-        int expectedHeartCount = 2;
+        int expectedHeartCount = 1;
         assertEquals(expectedHeartCount, hashMap.get("heartCount"));
-       Heart heart = heartService.heartDelete(dto.getPostId(), testMember2.getId());
+       Heart heart = heartService.heartDelete(responseDto2.getPostId(), testMember2.getId());
 
        // null이 아니면 좋아요 취소
         assertThat(heart).isNotNull();
@@ -98,10 +109,10 @@ class HeartServiceTest {
     @Test
     void getHeart() {
 
-        heartService.heartInsert(dto.getPostId(), testMember.getId());
-        heartService.heartInsert(dto.getPostId(), testMember2.getId());
+        heartService.heartInsert(responseDto.getPostId(), testMember.getId());
+        heartService.heartInsert(responseDto.getPostId(), testMember2.getId());
 
-        HashMap<String, Object> hashMap =  heartService.getHeart(dto.getPostId());
+        HashMap<String, Object> hashMap =  heartService.getHeart(responseDto.getPostId());
         int expectedHeartCount = 2;
         boolean expectedCheck = false;
         assertEquals(expectedHeartCount, hashMap.get("heartCount"));
