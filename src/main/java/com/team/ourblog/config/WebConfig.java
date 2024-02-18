3a2743e14cff294a07297f3fef7e1f988c545bf1
Seoha95 <serha95@naver.com>
2024-02-18 20:56:31 +0900

    Fix: 프론트엔드 주소 변경

--- src/main/java/com/team/ourblog/config/WebConfig.java
+++ src/main/java/com/team/ourblog/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("https://ourblog-five.vercel.app/")
+                        .allowedOriginPatterns("https://ourblog-beta.vercel.app/")
                         .allowedHeaders("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE","PATCH")
                         .exposedHeaders("Authorization");
